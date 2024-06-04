1b47b3e345a37203cf2eeb68505bfe588b7dc2d9
tynoerm <tinomutendaishemutemaringa@gmail.com>
2024-06-04 22:10:39 +0200

    Add files via upload

---
+++ erp/frontend/src/components/AdminUserManagement.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { MdDashboard } from "react-icons/md";
+import nav from "../../images/nav.jpeg";
+
+const AdminUserManagement = () => {
+  const navbarStyle = {
+    backgroundImage: `url(${nav})`, // Set the background image
+    backgroundSize: "cover", // Ensure the image covers the entire navbar
+    backgroundPosition: "center", // Center the background image
+    color: "black", // Set text color
+  };
+  return (
+    <div>
+            <nav
+        className="navbar bg-body-tertiary bg-dark border-bottom border-body"
+        style={navbarStyle}
+      >
+        <a className="navbar-brand" style={{ color: "white" }}>
+          <b>
+            {" "}
+            <MdDashboard /> &nbsp;ADMIN USER MANAGEMENT{" "}
+          </b>
+        </a>
+      </nav>
+
+
+
+
+      
+    </div>
+  )
+}
+ 
+export default AdminUserManagement;
