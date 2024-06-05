962aea96ade57c4f56e8b535cb781476c6f736c7
tynoerm <tinomutendaishemutemaringa@gmail.com>
2024-06-05 15:03:17 +0200

    Add files via upload

--- erp/frontend/src/FinanceAccounting/Manager/DashboardManager.js
+++ erp/frontend/src/FinanceAccounting/Manager/DashboardManager.js
@@ -1,15 +1,55 @@
-import React from "react";
 import { Link } from "react-router-dom";
 import { MdDashboard } from "react-icons/md";
 import nav from "../../images/nav.jpeg";
+import React, { useState, useEffect } from "react";
+import { Button, Modal } from "react-bootstrap";
+import axios from "axios";
 
 const FinanceAccountingDashboard = () => {
+  const [productionOrders, setProductionOrders] = useState([]);
+  const [materials, setMaterials] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("https://enterprise-resource-planning.onrender.com/productionorders/")
+      .then((res) => {
+        setProductionOrders(res.data.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
+  useEffect(() => {
+    axios
+      .get("https://enterprise-resource-planning.onrender.com/matrials/")
+      .then((res) => {
+        setProductionOrders(res.data.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
   const navbarStyle = {
     backgroundImage: `url(${nav})`, // Set the background image
     backgroundSize: "cover", // Ensure the image covers the entire navbar
     backgroundPosition: "center", // Center the background image
     color: "black", // Set text color
   };
+
+  const [show, setShow] = useState(false);
+  const handleShow = () => setShow(true);
+  const handleClose = () => setShow(false);
+
+  const [show1, setShow1] = useState(false);
+  const handleShow1 = () => setShow1(true);
+  const handleClose1 = () => setShow1(false);
+
+  const [show2, setShow2] = useState(false);
+  const handleShow2 = () => setShow2(true);
+  const handleClose2 = () => setShow2(false);
+
   return (
     <div>
       <nav
@@ -87,6 +127,148 @@ const FinanceAccountingDashboard = () => {
                   {" "}
                   Next{" "}
                 </Link>
+              </div>
+            </div>
+          </div>
+
+          <div class="col mb-4 shadow-sm p-3 mb-5 bg-body rounded">
+            <div class="card shadow p-3 mb-5 bg-body rounded">
+              <div class="card-body">
+                <h5 class="card-title">CHECK PRODUCTION MODULE</h5>
+                <p class="card-text">
+                  {" "}
+                  You can also have access to : production orders & bill of
+                  materials(materials required for production)
+                </p>
+                <Button variant="btn btn-primary" onClick={handleShow}>
+                  SELECT OPTION
+                </Button>
+
+                <Modal
+                  show={show}
+                  onHide={handleClose}
+                  backdrop="static"
+                  keyboard={false}
+                  size="auto"
+                  aria-labelledby="example-modal-sizes-title-lg"
+                >
+                  <Modal.Header closeButton>
+                    <Modal.Title>SELECT AN OPTION</Modal.Title>
+                  </Modal.Header>
+
+                  <Modal.Body>
+                    <div className="form-wrapper">
+                      <form>
+                        <div class="d-grid gap-2">
+                          <button
+                            class="btn btn-success"
+                            type="button"
+                            onClick={handleShow1}
+                          >
+                            PRODUCTION ORDERS
+                          </button>
+                          <button
+                            class="btn btn-success"
+                            type="button"
+                            onClick={handleShow2}
+                          >
+                            MATERIALS REQUIRED FOR PRODUCTION
+                          </button>
+                        </div>
+                      </form>
+                    </div>
+                  </Modal.Body>
+
+                  <Modal.Footer></Modal.Footer>
+                </Modal>
+
+                <Modal
+                  show={show1}
+                  onHide={handleClose1}
+                  backdrop="static"
+                  keyboard={false}
+                  size="lg"
+                  aria-labelledby="example-modal-sizes-title-lg"
+                >
+                  <Modal.Header closeButton>
+                    <Modal.Title>PRODUCTION ORDERS</Modal.Title>
+                  </Modal.Header>
+                  <Modal.Body>
+                    <div className="form-wrapper">
+                      <form>
+                        <div class="d-grid gap-2">
+                          <table className="table table-stripped">
+                            <thead>
+                              <tr>
+                                <th>Order ID</th>
+                                <th>Quantity</th>
+                                <th>Start Date</th>
+                                <th>status</th>
+                              </tr>
+                            </thead>
+                            <tbody>
+                              {productionOrders.map(
+                                (productionOrder, index) => (
+                                  <tr key={index}>
+                                    <td>{productionOrder.order_id}</td>
+                                    <td>{productionOrder.quantity}</td>
+                                    <td>{productionOrder.start_date}</td>
+                                    <td>{productionOrder.status}</td>
+                                  </tr>
+                                )
+                              )}
+                            </tbody>
+                          </table>
+                        </div>
+                      </form>
+                    </div>
+                  </Modal.Body>
+                  <Modal.Footer></Modal.Footer>
+                </Modal>
+
+                <Modal
+                  show={show2}
+                  onHide={handleClose2}
+                  backdrop="static"
+                  keyboard={false}
+                  size="lg"
+                  aria-labelledby="example-modal-sizes-title-lg"
+                >
+                  <Modal.Header closeButton>
+                    <Modal.Title>BILL OF MATERIALS</Modal.Title>
+                  </Modal.Header>
+
+                  <Modal.Body>
+                    <div className="form-wrapper">
+                      <form>
+                        <div class="d-grid gap-2">
+                          <table className="table table-stripped">
+                            <thead>
+                              <tr>
+                                <th>component_id</th>
+                                <th>quantity</th>
+                                <th>scrap_factor</th>
+                                <th>unit_ofmeasure</th>
+                              </tr>
+                            </thead>
+                            <tbody>
+                              {materials.map((material, index) => (
+                                <tr key={index}>
+                                  <td>{material.component_id}</td>
+                                  <td>{material.quantity}</td>
+                                  <td>{material.scrap_factor}</td>
+                                  <td>{material.unit_ofmeasure}</td>
+                                </tr>
+                              ))}
+                            </tbody>
+                          </table>
+                        </div>
+                      </form>
+                    </div>
+                  </Modal.Body>
+                  <Modal.Footer></Modal.Footer>
+                </Modal>
+
               </div>
             </div>
           </div>
